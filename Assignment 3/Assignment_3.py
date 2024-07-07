a91a4b8f43b7c557c592e38a15c7f3bbf1f65365
Prakash Batra <Prakash Batra@PRAKASHBATRA>
2024-07-07 18:24:54 -0400

    New file added for Assginment 3

---
+++ Assignment 3/Assignment_3.py
@@ -0,0 +1,55 @@
+# Importing required libraries
+
+# pip install selenium
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Setting up webdriver
+driver = webdriver.Chrome()
+
+# Navigating to https://www.conestogac.on.ca/
+driver.get("https://www.conestogac.on.ca/")
+time.sleep(2)
+
+# Maximize browser window
+driver.maximize_window()
+
+# Clicking on programs-and-courses menu
+programs_and_courses = driver.find_element("xpath","/html/body/div[1]/div[2]/div[2]/div/div/div[2]/div[1]/a[1]")
+programs_and_courses.click()
+time.sleep(2)
+
+# Clicking on Full-Time Programs button
+full_time_programs = driver.find_element("xpath","/html/body/div[1]/div[3]/div[2]/div/div[1]/a")
+full_time_programs.click()
+time.sleep(2)
+
+# Clicking on International students checkbox
+international_students = driver.find_element("id","DeliveryGroups")
+international_students.click()
+time.sleep(2)
+
+# Clicking on Apply filters button
+apply_filters = driver.find_element("xpath","/html/body/div[1]/div[3]/div[2]/div/div/div[2]/form/div/div[3]/div/div[1]/div[3]/div[1]/button/span/span[1]")
+apply_filters.click()
+time.sleep(2)
+
+# Clicking on first program in the filtered results
+first_program = driver.find_element("xpath","/html/body/div[1]/div[3]/div[2]/div/div/div[2]/form/div/div[2]/div[1]/div/div/ul/li[1]/a/span/span[1]")
+first_program.click()
+time.sleep(2)
+
+# Clicking on first program in the filtered results
+admissions = driver.find_element("xpath","/html/body/div[1]/div[3]/div[2]/div/div/div[1]/div[2]/div/ul/li[2]/a")
+admissions.click()
+time.sleep(2)
+
+# Clicking on Apply Now
+apply_now = driver.find_element("xpath","/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div/div[1]/button/span")
+apply_now.click()
+time.sleep(2)
+
+# Closing the webdriver
+#driver.close()
